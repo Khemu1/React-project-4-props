7c4782962dc4218f085e64b718f3a7f84f333f58
khemu_1 <alghost2004@gmail.com>
2023-09-12 13:11:51 +0300

    project-2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function Header() {
 					<img src={logo} alt="logo" />
 					<h1>ReactFacts</h1>
 				</div>
-				<div className="project-num">React Course- Project 1</div>
+				<div className="project-num">React Course- Project 2</div>
 			</nav>
 		</header>
 	);
