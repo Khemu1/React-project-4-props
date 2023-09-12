c1279f23913edf1268c8d789f22622b93ab392c4
khemu_1 <alghost2004@gmail.com>
2023-09-12 15:24:36 +0300

    Done

---
+++ src/Js/website.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import MainCont from "./MainCont";
+import Footer from "./footer";
+function Website() {
+	return (
+		<div className="parent">
+			<Header />
+			<MainCont />
+			<Footer />
+		</div>
+	);
+}
+export default Website;
--- src/index.js
+++ src/index.js
@@ -1,38 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./App.css";
-import Header from "./App";
-
-// import App2 from "./App2";
-
-function MainCont() {
-	return (
-		<div>
-			<h1>Fun facts about React</h1>
-			<ol>
-				<li>Was first released in 2013</li>
-				<li>Was originally created by Jordan Walke</li>
-				<li>Has well over 100K stars on GitHub</li>
-				<li>Is maintained by Facebook</li>
-				<li>Powers thousands of enterprise apps, including mobile apps</li>
-			</ol>
-		</div>
-	);
-}
-
-function Cont() {
-	return (
-		<div className="parent">
-			<Header />
-			<div className="cont">
-				<MainCont />
-			</div>
-		</div>
-	);
-}
+import "./index.css";
+import Website from "./Js/website";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Cont />);
+root.render(<Website />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
