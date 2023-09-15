2cbf7bf4d2b987257174b5c769d71b3e8ef507f8
khemu_1 <alghost2004@gmail.com>
2023-09-15 18:54:59 +0300

    Done

--- src/Js/website.js
+++ src/Js/website.js
@@ -1,20 +1,16 @@
 import React from "react";
-import Nav from "./nav";
-import Pics from "./pics";
-import Info from "./info";
-import Cards from "./cards";
 import data from "./data";
-
+import Nav from "./nav";
+import Card from "./card";
 function Website() {
 	const elements = data.map((e) => {
-		return <Cards {...e} key={e.id} />;
+		return <Card {...e} key={e.id} />;
 		// it's recommended for each child to have a key
 	});
 	return (
 		<div className="parent">
 			<Nav />
-			<Pics />
-			<Info />
+
 			<div className="cards-cont">{elements}</div>
 		</div>
 	);
